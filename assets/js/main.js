e73dfd793d3d681786c4bec5f677f6e107270287
chaulagainkapil <chaukapil99@gmail.com>
2023-05-22 17:46:33 +0545

    navbar complete

--- assets/js/main.js
+++ assets/js/main.js
@@ -33,4 +33,52 @@ $(function () {
       $(".counter span").html(count);
     }
   });
+
+  //cartopening
+  $(".nav__right--cart").click(function () {
+    $(".cartBox").toggleClass("d-block");
+    $(".cartBox").toggleClass("d-none");
+  });
+
+  //add to cart
+  let initialItems= parseInt($(".numItems").text());
+
+  $(document).on("click", ".addCart", function(){
+    console.log("addddd");
+    let numProd = parseInt($(".counter__count").text());
+    let basePrice = 125;
+    let finalPrice = 0;
+    if(numProd> 0){
+        $(".cartBox__body--empty ").addClass("d-none");
+        $(".cartBox__body--notE").removeClass("d-none");
+        $(".cartBox__body--notE").addClass("d-block");
+        initialItems+=numProd;
+        $(".numItems").text(initialItems + " = ");
+        finalPrice = parseInt(basePrice) * parseInt(initialItems);
+        console.log(finalPrice);
+        $(".totalPrice").text( "$"+finalPrice+".00");
+    }
+  })
+
+  //deleteitem
+  $(document).on("click", ".deleteB", function(){
+    $(".cartBox__body--empty ").removeClass("d-none")
+    $(".cartBox__body--empty ").addClass("d-block");
+    $(".cartBox__body--notE").removeClass("d-block");
+    $(".cartBox__body--notE").addClass("d-none");
+    initialItems =0;
+  })
+
+
+  //navbar open close
+  $(document).on("click", ".nav__left--button--hamburger", function(){
+    $(".nav__left--button--hamburger").css("display", "none");
+    $(".nav__left--button--close").css("display", "block");
+    $(".nav__left--menu").css("display", "block");
+  })
+  $(document).on("click", ".nav__left--button--close", function(){
+    $(".nav__left--button--hamburger").css("display", "block");
+    $(".nav__left--button--close").css("display", "none");
+    $(".nav__left--menu").css("display", "none");
+  })
 });
