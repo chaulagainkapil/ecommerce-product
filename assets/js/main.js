104737db1828fa2d6488cd680908d4db7b3651b8
chaulagainkapil <chaukapil99@gmail.com>
2023-05-23 11:04:01 +0545

    Lightbox added

--- assets/js/main.js
+++ assets/js/main.js
@@ -10,10 +10,10 @@ $(function () {
     console.log(t);
     $(".p" + t + "_big").addClass("d-block");
     $(".p" + t + "_big").removeClass("d-none");
-    $(".main__card--image--upper img")
+    $(".main__card--image--upper a img")
       .not(".p" + t + "_big")
       .addClass("d-none");
-    $(".main__card--image--upper img")
+    $(".main__card--image--upper a img")
       .not(".p" + t + "_big")
       .removeClass("d-block");
   });
@@ -41,44 +41,49 @@ $(function () {
   });
 
   //add to cart
-  let initialItems= parseInt($(".numItems").text());
+  let initialItems = parseInt($(".numItems").text());
 
-  $(document).on("click", ".addCart", function(){
+  $(document).on("click", ".addCart", function () {
     console.log("addddd");
     let numProd = parseInt($(".counter__count").text());
     let basePrice = 125;
     let finalPrice = 0;
-    if(numProd> 0){
-        $(".cartBox__body--empty ").addClass("d-none");
-        $(".cartBox__body--notE").removeClass("d-none");
-        $(".cartBox__body--notE").addClass("d-block");
-        initialItems+=numProd;
-        $(".numItems").text(initialItems + " = ");
-        finalPrice = parseInt(basePrice) * parseInt(initialItems);
-        console.log(finalPrice);
-        $(".totalPrice").text( "$"+finalPrice+".00");
+    if (numProd > 0) {
+      $(".cartBox__body--empty ").addClass("d-none");
+      $(".cartBox__body--notE").removeClass("d-none");
+      $(".cartBox__body--notE").addClass("d-block");
+      initialItems += numProd;
+      $(".numItems").text(initialItems + " = ");
+      finalPrice = parseInt(basePrice) * parseInt(initialItems);
+      console.log(finalPrice);
+      $(".totalPrice").text("$" + finalPrice + ".00");
     }
-  })
+  });
 
   //deleteitem
-  $(document).on("click", ".deleteB", function(){
-    $(".cartBox__body--empty ").removeClass("d-none")
+  $(document).on("click", ".deleteB", function () {
+    $(".cartBox__body--empty ").removeClass("d-none");
     $(".cartBox__body--empty ").addClass("d-block");
     $(".cartBox__body--notE").removeClass("d-block");
     $(".cartBox__body--notE").addClass("d-none");
-    initialItems =0;
-  })
-
+    initialItems = 0;
+  });
 
   //navbar open close
-  $(document).on("click", ".nav__left--button--hamburger", function(){
-    $(".nav__left--button--hamburger").css("display", "none");
+  $(document).on("click", ".nav__left--button--hamburger", function () {
     $(".nav__left--button--close").css("display", "block");
     $(".nav__left--menu").css("display", "block");
-  })
-  $(document).on("click", ".nav__left--button--close", function(){
-    $(".nav__left--button--hamburger").css("display", "block");
+    $("html").addClass("lock-scroll");
+  });
+  //navbar close
+  $(document).on("click", ".nav__left--button--close", function () {
     $(".nav__left--button--close").css("display", "none");
     $(".nav__left--menu").css("display", "none");
-  })
+    $("html").removeClass("lock-scroll");
+  });
+
+  //fancybox
+  Fancybox.bind('[data-fancybox="gallery"]', {
+    // Your custom options
+  });
 });
